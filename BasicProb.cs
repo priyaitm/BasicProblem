f5637e22c084f4b80335035f23ea1aaf0dc0ff7c
priya <priyachauhan47365@gmail.com>
2023-07-13 10:48:52 -0600

    Added : Basic Problem

---
+++ BasicProb.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PositiveNum
+{
+    internal  class BasicProb
+
+    {
+        public void CheckNumber(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine("number is positive");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("number is negative");
+            }
+        }
+
+        public void CheckEvenOdd(int n)
+        {
+            if(n % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+        }
+
+        public void CheckLeapYear( int year)
+        {
+            if (((year % 400 == 0))  || ((year % 4 == 0) && (year % 100 != 0))){
+                Console.WriteLine("year is Leap Year");
+                Console.ReadLine();
+            } else { Console.WriteLine("Year is not Leap Year"); }
+
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PositiveNum
+{
+    internal class Program
+
+    {
+      static void Main(string[] args)
+
+        {
+            Console.WriteLine("Enter the Number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            BasicProb obj = new BasicProb();
+           // obj.CheckNumber(n);
+           // obj.CheckEvenOdd(n);
+            obj.CheckLeapYear(n);
+
+            Console.ReadLine();
+         }
+    }
+}
